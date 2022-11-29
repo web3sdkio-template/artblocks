3841377100d3441d860cecf8bedac4c952711872
zendqgoo <zendqgoo@outlook.com>
2022-11-29 11:06:28 +0800

    First commit

---
+++ generative-art-server/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import handleBarsExpress from "express-handlebars";
+import path from "path";
+
+import { getScript } from "./controllers/sketches.js";
+import { saveImage } from "./controllers/images.js";
+import { Web3sdkioStorage } from "@web3sdkio/storage";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const handleBars = handleBarsExpress.create({
+  layoutsDir: path.dirname(".") + "/views/layouts",
+  extname: "hbs",
+  defaultLayout: "index",
+});
+
+app.engine("hbs", handleBars.engine);
+app.set("view engine", "hbs");
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(express.static("public"));
+
+const storage = new Web3sdkioStorage();
+const tokenImages = {};
+
+app.get("/token/:tokenId", async (req, res) => {
+  const hash = await getScript(req.params.tokenId);
+
+  if (!tokenImages[`img_${req.params.tokenId}`]) {
+    const buf = saveImage(hash, req.params.tokenId);
+
+    const result = await storage.upload(buf);
+
+    tokenImages[`img_${req.params.tokenId}`] = await storage.resolveScheme(
+      result
+    );
+  }
+
+  res.render("piece", {
+    scriptName: `mySketch.js`,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
